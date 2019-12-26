112a1d27315214559a44adc0512755d2a9eb4416
DzungHoang <DzungHoang@gitlab.com>
2019-12-26 23:11:37 +0700

    add download script

---
+++ data/processed/download.sh
@@ -0,0 +1,6 @@
+for i in mesos usergrid appceleratorstudio aptanastudio titanium duracloud bamboo clover jirasoftware moodle datamanagement mule mulestudio springxd talenddataquality talendesb 
+do 
+    wget "https://github.com/hoangdzung/datasets/blob/master/storypoint/IEEE%20TSE2018/Deep-SE/data/${i}.dict.pkl.gz"
+    wget "https://github.com/hoangdzung/datasets/blob/master/storypoint/IEEE%20TSE2018/Deep-SE/data/${i}.pkl.gz"
+done
+ 
